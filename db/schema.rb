d89cf4e1f510ac4e87311e29b1749b5f8b733f14
Daniel Llano <daniel.llano.a@gmail.com>
2016-07-08 10:14:19 -0500

    Add status column to purchases

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160630231955) do
+ActiveRecord::Schema.define(version: 20160708151229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20160630231955) do
     t.integer  "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
   add_index "purchases", ["product_id"], name: "index_purchases_on_product_id", using: :btree
