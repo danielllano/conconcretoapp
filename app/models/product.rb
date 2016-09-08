a7085eb56f72406a35e9e50334e22b0334aebcb4
Daniel Llano <daniel.llano.a@gmail.com>
2016-09-07 22:47:39 -0500

    Styled products images sizes and catalog

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,12 +2,12 @@ class Product < ActiveRecord::Base
   has_many :purchases
   has_many :users, :through => :purchases
 
-  has_attached_file :image1, styles: { medium: "x450", small: "x150" }
+  has_attached_file :image1, styles: { medium: "x600", small: "x200" }
   validates_attachment_content_type :image1, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :image2, styles: { medium: "x450", small: "x150" }
+  has_attached_file :image2, styles: { medium: "x600", small: "x200" }
   validates_attachment_content_type :image1, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :image3, styles: { medium: "x450", small: "x150" }
+  has_attached_file :image3, styles: { medium: "x600", small: "x200" }
   validates_attachment_content_type :image1, content_type: /\Aimage\/.*\z/
 end
