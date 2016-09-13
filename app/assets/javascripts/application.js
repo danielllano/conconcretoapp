ddbc2549f413d0e53266dbb04c2f104f0dbbb7eb
Daniel Llano <daniel.llano.a@gmail.com>
2016-09-12 22:54:27 -0500

    Fix turbolinks issue with home carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require jquery.turbolinks
 //= require jquery-1.11.0.min
 //= require bootstrap.min
 //= require jquery.scrollTo.min
